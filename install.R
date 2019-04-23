b89e167109c99385b615ccfa88484a218d5a4ba4
William Baker <william.l.baker2@gmail.com>
2019-04-22 22:06:16 -0500

    Add packages to automated install.

--- install.R
+++ install.R
@@ -3,3 +3,13 @@ install.packages("rmarkdown")
 install.packages("httr")
 install.packages("shinydashboard")
 install.packages('leaflet')
+#
+# prerequisites of osfr which I prefer to load manually for diagnostic purposes
+#
+install.packages('crul')
+install.packages('fs')
+install.packages('httpcode')
+install.packages('triebeard')
+install.packages('urltools')
+install.packages('remotes')
+remotes::install_github("centerforopenscience/osfr")
